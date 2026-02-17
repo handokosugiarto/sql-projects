c0f88f21e22c0d8ccb43a10a6b00e4caa0b4933b
Handoko Sugiarto <handokosugiarto82259@gmail.com>
2026-02-16 17:08:56 -0800

    Create Database_Exploration.sql

---
+++ scripts/Superstore/Database_Exploration.sql
@@ -0,0 +1,17 @@
+/*======================================================================================
+Database Exploration
+The purpose of this exploration is to have basic understanding about database, columns, 
+tables, views, and structure of database.
+========================================================================================*/
+
+--- Explore All Objects in the Database. 
+
+SELECT * FROM INFORMATION_SCHEMA.TABLES
+
+--- Explore All columns in the Database
+
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME ='Superstore'
+
+
+
