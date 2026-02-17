5341618d486c0e9e849d7203381089128bd4ce15
Handoko Sugiarto <handokosugiarto82259@gmail.com>
2026-02-16 19:50:21 -0800

    Create SQL_create_table_cleaned2.sql

---
+++ scripts/Amazon/SQL_create_table_cleaned2.sql
@@ -0,0 +1,33 @@
+/*
+==================================================================================
+DDL Scripts: create table Amazon products sales data cleaned2
+
+==================================================================================
+Script purpose:
+	This script creates table in amazon schema by dropping existing table if
+	they already exist.
+	Run this scripts to re-define DDL structure of cleaned2 table
+==================================================================================
+*/
+
+IF OBJECT_ID ('amazon.amazon_sales_data_cleaned2', 'U') IS NOT NULL
+DROP TABLE amazon.amazon_sales_data_cleaned2;
+CREATE TABLE amazon.amazon_sales_data_cleaned2 (
+	product_key INT,
+	product_title NVARCHAR(MAX),
+	product_rating FLOAT,
+	total_reviews INT,
+	purchased_last_month FLOAT,
+	discounted_price FLOAT,
+	original_price FLOAT,
+	is_best_seller NVARCHAR(50),
+	is_sponsored NVARCHAR(50),
+	has_coupon NVARCHAR(50),
+	buy_box_availability NVARCHAR(50),
+	delivery_date DATE,
+	sustainability_tags NVARCHAR(50),
+	product_image_url NVARCHAR(100),
+	product_page_url NVARCHAR(800),
+	data_collected_at DATETIME,
+	product_category NVARCHAR(50),
+	discount_percentage FLOAT);
